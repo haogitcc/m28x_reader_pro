970798a908f9c77dc9a41d397921cd088daa4137
haogitcc@163.com <haogitcc@163.com>
2020-03-12 01:47:38 -0700

    Init Sichuang when read a tag warning
    1_读到标签报警

---
+++ 0005_SichuangFangdaoBaojing/1_读到标签报警/m6e-reader/module_init.h
@@ -0,0 +1,18 @@
+#ifndef _MODULE_INIT_H
+#define _MODULE_INIT_H
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+int module_init();
+void module_startReading();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SERIAL_READER_IMP_H */
+
